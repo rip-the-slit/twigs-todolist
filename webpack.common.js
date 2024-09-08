66c4518d16e70426a3ed44b5f6ce677985a0f3ee
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-09-08 19:49:10 -0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPluging = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPluging({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+}
