20bd08703b912151a5fef9d0eb655facadc61098
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-09-15 03:33:42 +0000

    Object with a method to populate the branch list in the sidebar

--- src/dom-manipulation.js
+++ src/dom-manipulation.js
@@ -1,9 +1,13 @@
 export { clear, build}
 
-const clear = (function() {
-    const getNode = (tag) => {
-        return document.querySelector(tag)
+const getNode = (tag) => {
+    return document.querySelector(tag)
+}
+const createElement = (element) => {
+        return document.createElement(element)
     }
+
+const clear = (function() {
     const branchListNode = getNode("#branch-list")
     const clear = (item) => {
         while(item.lastChild) {
@@ -18,7 +22,43 @@ const clear = (function() {
 
 const build = (function() {
     const branchListItem = (obj) => {
+        const branchListNode = getNode("#branch-list")
+        const itemName = obj.name
         const subItems = obj.allTwigs()
+
+        const branch = createElement("div")
+        const h3 = createElement("h3")
+        const ul = createElement("ul")
+
+        for (const item of subItems) {
+            const li = createElement("li")
+            const checkbox = createElement("div")
+            const checkboxInner = createElement("div")
+            const itemStatus = item.status
+            const span = createElement("span")
+
+            checkbox.classList.add("checkbox")
+            if (itemStatus == "due") {checkbox.toggleAttribute("checked", true)}
+            li.addEventListener("click", (e) => {
+                checkbox.toggleAttribute("checked")
+                item.toggleStatus()
+            })
+
+            span.textContent = item.name
+            
+            checkbox.appendChild(checkboxInner)
+            li.appendChild(checkbox)
+            li.appendChild(span)
+            ul.appendChild(li)
+        }
+
+        branch.classList.add("branch")
+
+        h3.textContent = itemName
+
+        branch.appendChild(h3)
+        branch.appendChild(ul)
+        branchListNode.appendChild(branch)
     }
     return { branchListItem }
 })()
